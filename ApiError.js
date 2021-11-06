4ebe91288251dbf09b6bee93339de8fd9731dee8
tomfeli <fiszsontom@gmail.com>
2021-11-06 16:29:19 -0300

    primer commit completo

--- ApiError.js
+++
@@ -1,8 +0,0 @@
-
-function crearErrorDeAPI(msg) {
-    const err = new Error(msg)
-    err.type = 'API_ERROR'
-    return err
-}
-
-export { crearErrorDeAPI }
--- index.js
+++
@@ -1,3 +0,0 @@
-import { realizarConversion } from './Conversor'
-
-console.log(realizarConversion(ARS,USD,1));
